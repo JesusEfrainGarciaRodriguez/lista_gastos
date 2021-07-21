8ff4e41c8294c134455f2a3ebf16eef97813ab2f
Efrain Garcia <66291025+JesusEfrainGarciaRodriguez@users.noreply.github.com>
2021-07-21 17:05:48 -0600

    Agregando fondo y favicon
    + Se agrego la librería react helmet para agregar el encabezado

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+// Librerias
 import WebFont from 'webfontloader';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {Helmet} from "react-helmet";
 
 // Elementos
 import Contenedor from './elementos/Contenedor';
+import Fondo from './elementos/Fondo';
 
 // Componentes
 import EditarGasto from './componentes/EditarGasto';
@@ -14,6 +18,9 @@ import GastosPorCategoria from './componentes/GastosPorCategoria';
 import InicioSesion from './componentes/InicioSesion';
 import ListaDeGastos from './componentes/ListaDeGastos';
 import RegistroUsuarios from './componentes/RegistroUsuarios';
+
+// Img
+import favicon from './img/logo.png';
 
 WebFont.load({
   google: {
@@ -24,18 +31,26 @@ WebFont.load({
 
 const Index = () => {
   return (
-    <BrowserRouter>
-      <Contenedor>
-        <Switch>
-          <Route path="/iniciar-sesion" component={InicioSesion}/>
-          <Route path="/crear-cuenta" component={RegistroUsuarios}/>
-          <Route path="/categorias" component={GastosPorCategoria}/>
-          <Route path="/lista" component={ListaDeGastos}/>
-          <Route path="/editar/:id" component={EditarGasto}/>
-          <Route path="/" component={App}/>
-        </Switch>
-      </Contenedor>
-    </BrowserRouter>
+    <>
+      <Helmet>
+        <link rel="shortcut icon" href={favicon} type="image/x-icon"/>
+      </Helmet>
+
+      <BrowserRouter>
+        <Contenedor>
+          <Switch>
+            <Route path="/iniciar-sesion" component={InicioSesion}/>
+            <Route path="/crear-cuenta" component={RegistroUsuarios}/>
+            <Route path="/categorias" component={GastosPorCategoria}/>
+            <Route path="/lista" component={ListaDeGastos}/>
+            <Route path="/editar/:id" component={EditarGasto}/>
+            <Route path="/" component={App}/>
+          </Switch>
+        </Contenedor>
+      </BrowserRouter>
+
+      <Fondo/>
+    </>
   );
 }
 
