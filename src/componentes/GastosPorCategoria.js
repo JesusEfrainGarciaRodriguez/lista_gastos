6227134b3364a74b29d27d7e0f0d04ebb09293ae
Efrain Garcia <66291025+JesusEfrainGarciaRodriguez@users.noreply.github.com>
2021-08-03 12:00:12 -0600

    Gastos por categoría

--- src/componentes/GastosPorCategoria.js
+++ src/componentes/GastosPorCategoria.js
@@ -7,8 +7,18 @@ import BarraTotalGastado from './BarraTotalGastado';
 // Elementos
 import { Header, Titulo } from '../elementos/Header';
 import BtnRegresar from '../elementos/BtnRegresar';
+import { ListaDeCategorias, ElementoListaCategorias, Categoria, Valor } from '../elementos/ElementosDeLista';
+import IconoCategoria from '../elementos/IconoCategoria';
+
+// Hooks
+import useObtenerGastosDelMesPorCategoria from '../hooks/useObtenerGastosDelMesPorCategoria';
+
+// Funcion
+import convertirAMoneda from '../funciones/convertirAMoneda';
 
 const GastosPorCategoria = () => {
+    const gastosPorCategoria = useObtenerGastosDelMesPorCategoria();
+
     return (
         <>
             <Helmet>
@@ -19,6 +29,20 @@ const GastosPorCategoria = () => {
                 <BtnRegresar/>
                 <Titulo>Gastos por Categoría</Titulo>
             </Header>
+
+            <ListaDeCategorias>
+                { gastosPorCategoria.map((elemento, index) => {
+                    return (
+                        <ElementoListaCategorias key={index}>
+                            <Categoria>
+                                <IconoCategoria id={elemento.categoria}/>
+                                {elemento.categoria}
+                            </Categoria>
+                            <Valor>{convertirAMoneda(elemento.cantidad)}</Valor>
+                        </ElementoListaCategorias>
+                    );
+                })}
+            </ListaDeCategorias>
 
             <BarraTotalGastado />
         </>
